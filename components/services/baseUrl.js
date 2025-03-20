594ba0b1346ebf9668b6b9f6188dda281aaabc23
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-03-21 03:20:59 +0600

    base url change

--- components/services/baseUrl.js
+++ components/services/baseUrl.js
@@ -1,5 +1,5 @@
 //  const baseUrl = 'https://api.v1.estarch.online'
 //  const baseUrl = 'https://api.showroom.estarch.com.bd'
-// const baseUrl = 'https://estarch-server-presonal.onrender.com'
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'https://estarch-server-presonal.onrender.com'
+// const baseUrl = 'http://localhost:5000'
 export default baseUrl;
