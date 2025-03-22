11139916a179e357b2fc82258097f2bf6e4da4c4
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-03-23 05:02:53 +0600

    api.showroom backend

--- components/services/baseUrl.js
+++ components/services/baseUrl.js
@@ -1,7 +1,7 @@
 //  const baseUrl = 'https://api.v1.estarch.online'
-//  const baseUrl = 'https://api.showroom.estarch.com.bd'
+ const baseUrl = 'https://api.showroom.estarch.com.bd'
 // const baseUrl = 'https://estarch-server-presonal.onrender.com'
 // const baseUrl = 'http://localhost:5000'
 // const baseUrl = 'https://api.test.backend.estarch.com.bd'
-const baseUrl = 'http://185.227.134.170:5000'
+// const baseUrl = 'http://185.227.134.170:5000'
 export default baseUrl;
